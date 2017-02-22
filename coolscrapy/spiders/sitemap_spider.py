906b174cd4023da75e97df64a78962483a11d71f
ningskyer <ningskyer@qq.com>
2017-02-22 17:32:44 +0800

    增加sitemap类型spider

---
+++ coolscrapy/spiders/sitemap_spider.py
@@ -0,0 +1,24 @@
+from scrapy.contrib.spiders import SitemapSpider
+
+class SiteSpider(SitemapSpider):
+    sitemap_urls = ['http://www.example.com/sitemap.xml']
+    sitemap_rules = [
+        ('/product/', 'parse_product'),
+        ('/category/', 'parse_category'),
+    ]
+
+    other_urls = ['http://www.example.com/about']
+
+    def start_requests(self):
+        requests = list(super(MySpider, self).start_requests())
+        requests += [scrapy.Request(x, self.parse_other) for x in self.other_urls]
+        return requests
+
+    def parse_product(self, response):
+        pass # ... scrape product ...
+
+    def parse_category(self, response):
+        pass # ... scrape category ...
+        
+    def parse_other(self, response):
+        pass # ... scrape other here ...
